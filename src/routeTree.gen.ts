a7da96d78f621c1ddd5a3d9b0bcce9e80eb718f0
Yy0839-blip <Yy0839@naver.com>
2026-09-05 16:07:43 +0900

    Remove unused broken space detail route

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -10,7 +10,6 @@ import { Route as ProfileRouteImport } from './routes/profile'
 import { Route as ProgramsRouteImport } from './routes/programs'
 import { Route as ProgramsNearbyRouteImport } from './routes/programs/nearby'
 import { Route as SpacesRouteImport } from './routes/spaces'
-import { Route as SpacesSpaceIdRouteImport } from './routes/spaces/$spaceId'
 import { Route as MeetupRouteImport } from './routes/meetup'
 import { Route as MusicRouteImport } from './routes/music'
 
@@ -21,12 +20,11 @@ const MissionsRoute = MissionsRouteImport.update({ id: '/missions', path: '/miss
 const ProfileRoute = ProfileRouteImport.update({ id: '/profile', path: '/profile', getParentRoute: () => rootRouteImport } as any)
 const ProgramsNearbyRoute = ProgramsNearbyRouteImport.update({ id: '/programs/nearby', path: '/nearby', getParentRoute: () => ProgramsRoute } as any)
 const ProgramsRoute = ProgramsRouteImport.update({ id: '/programs', path: '/programs', getParentRoute: () => rootRouteImport } as any)._addFileChildren({ ProgramsNearbyRoute })
-const SpacesSpaceIdRoute = SpacesSpaceIdRouteImport.update({ id: '/spaces/$spaceId', path: '/$spaceId', getParentRoute: () => SpacesRoute } as any)
-const SpacesRoute = SpacesRouteImport.update({ id: '/spaces', path: '/spaces', getParentRoute: () => rootRouteImport } as any)._addFileChildren({ SpacesSpaceIdRoute })
+const SpacesRoute = SpacesRouteImport.update({ id: '/spaces', path: '/spaces', getParentRoute: () => rootRouteImport } as any)
 const MeetupRoute = MeetupRouteImport.update({ id: '/meetup', path: '/meetup', getParentRoute: () => rootRouteImport } as any)
 const MusicRoute = MusicRouteImport.update({ id: '/music', path: '/music', getParentRoute: () => rootRouteImport } as any)
 
-export interface FileRoutesByFullPath { '/': typeof IndexRoute; '/community': typeof CommunityRoute; '/events': typeof EventsRoute; '/missions': typeof MissionsRoute; '/profile': typeof ProfileRoute; '/programs': typeof ProgramsRoute; '/programs/nearby': typeof ProgramsNearbyRoute; '/spaces': typeof SpacesRoute; '/spaces/$spaceId': typeof SpacesSpaceIdRoute; '/meetup': typeof MeetupRoute; '/music': typeof MusicRoute }
+export interface FileRoutesByFullPath { '/': typeof IndexRoute; '/community': typeof CommunityRoute; '/events': typeof EventsRoute; '/missions': typeof MissionsRoute; '/profile': typeof ProfileRoute; '/programs': typeof ProgramsRoute; '/programs/nearby': typeof ProgramsNearbyRoute; '/spaces': typeof SpacesRoute; '/meetup': typeof MeetupRoute; '/music': typeof MusicRoute }
 export interface FileRoutesByTo extends FileRoutesByFullPath {}
 export interface FileRoutesById extends FileRoutesByFullPath { __root__: typeof rootRouteImport }
 export interface FileRouteTypes { fileRoutesByFullPath: FileRoutesByFullPath; fullPaths: keyof FileRoutesByFullPath; fileRoutesByTo: FileRoutesByTo; to: keyof FileRoutesByTo; id: keyof FileRoutesById; fileRoutesById: FileRoutesById }
@@ -41,7 +39,6 @@ declare module '@tanstack/react-router' { interface FileRoutesByPath {
  '/programs': { id: '/programs'; path: '/programs'; fullPath: '/programs'; preLoaderRoute: typeof ProgramsRouteImport; parentRoute: typeof rootRouteImport }
  '/programs/nearby': { id: '/programs/nearby'; path: '/nearby'; fullPath: '/programs/nearby'; preLoaderRoute: typeof ProgramsNearbyRouteImport; parentRoute: typeof ProgramsRouteImport }
  '/spaces': { id: '/spaces'; path: '/spaces'; fullPath: '/spaces'; preLoaderRoute: typeof SpacesRouteImport; parentRoute: typeof rootRouteImport }
- '/spaces/$spaceId': { id: '/spaces/$spaceId'; path: '/$spaceId'; fullPath: '/spaces/$spaceId'; preLoaderRoute: typeof SpacesSpaceIdRouteImport; parentRoute: typeof SpacesRouteImport }
  '/meetup': { id: '/meetup'; path: '/meetup'; fullPath: '/meetup'; preLoaderRoute: typeof MeetupRouteImport; parentRoute: typeof rootRouteImport }
  '/music': { id: '/music'; path: '/music'; fullPath: '/music'; preLoaderRoute: typeof MusicRouteImport; parentRoute: typeof rootRouteImport }
 }}
